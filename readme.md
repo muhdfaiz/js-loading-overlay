f2a6a035f971ced0cc23c44ae97db54608c0de14
Faiz Halim <muhdfaizhalim@gmail.com>
2019-10-11 22:50:50 +0800

    Update webpack config, add travis ci config and coverall config

---
+++ readme.md
@@ -0,0 +1,1 @@
+## Javascript Loading Overlay
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
 const path = require("path");
 const TerserPlugin = require('terser-webpack-plugin');
+const env = process.env.NODE_ENV;
 
-module.exports = {
-    devtool: "source-map",
-    target: "async-node",
-    entry: ["./src/loading-overlay.js"],
-    output: {
-        filename: "dist/loading-overlay.js",
-        path: path.resolve(__dirname, '')
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                extractComments: true,
-                cache: true,
-                parallel: true,
-                sourceMap: true, // Must be set to true if using source-maps in production
-                terserOptions: {
-                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-                    extractComments: 'all',
-                    compress: {
-                        drop_console: true,
-                    },
-                    output: {
-                        comments: false,
-                    },
-                }
-            }),
-        ],
+const config = {
+    mode: env || 'development'
+};
+
+module.exports = (env, options) => {
+    return {
+        devtool: "source-map",
+        target: "async-node",
+        entry: ["./src/loading-overlay.js"],
+        output: {
+            filename: options.mode == 'developement' ? "dist/loading-overlay.js" : "dist/loading-overlay.min.js",
+            path: path.resolve(__dirname, '')
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }]
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: true,
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true, // Must be set to true if using source-maps in production
+                    terserOptions: {
+                        // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                        extractComments: 'all',
+                        compress: {
+                            drop_console: true,
+                        },
+                        output: {
+                            comments: false,
+                        },
+                    }
+                }),
+            ],
+        }
     }
 };
